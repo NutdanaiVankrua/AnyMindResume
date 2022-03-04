c51d7d18b7e7996fd4824066af3d1f3fd9f5b7b8
Nutdanai Vankrua <nutdanai.vankrua@gmail.com>
2022-03-04 16:16:21 +0700

    feature: prepare data structure for resume

--- app/src/main/java/com/example/anymindresume/screen/myresumes/Resume.kt
+++ app/src/main/java/com/example/anymindresume/screen/myresumes/Resume.kt
@@ -1,5 +1,52 @@
 package com.example.anymindresume.screen.myresumes
 
+import java.util.*
+
 data class Resume(
-    val name: String
+    val name: String,
+    val contact: Contact,
+    val experience: Experience,
+    val educations: List<Education>,
+    val projects: List<Project>
+)
+
+data class Contact(
+    val address: String,
+    val mobile: String,
+    val email: String
+)
+
+data class Experience(
+    val careerObjective: String,
+    val yearsOfExperience: Int,
+    val history: List<Organization>
+) {
+
+    data class Organization(
+        val name: String,
+        val start: Date,
+        val end: Date,
+        val description: String?
+    )
+
+}
+
+data class Education(
+    val classes: Class,
+    val passingYear: Int,
+    val cgpa: Float // max 2 floating points
+) {
+
+    enum class Class {
+        X, XII, GRADUATION
+    }
+
+}
+
+data class Project(
+    val name: String,
+    val teamSize: Int,
+    val summary: String,
+    val techStack: String,
+    val role: String
 )
